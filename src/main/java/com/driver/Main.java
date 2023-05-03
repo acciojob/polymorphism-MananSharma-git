a4e4ae536a8152153b4ac21cb61d84200862d588
MANAN SHARMA <115975287+MananSharma-git@users.noreply.github.com>
2023-05-04 00:24:29 +0530

    check2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,17 +5,21 @@ public class Main {
             public int product(int x,int y){
                 return x+y;
             }
-            public double product(double x,double y){
-                return x+y;
-            }
+           
             public int product(int x,int y,int z){
                 return x+y+z;
+            }
+      
+            public double product(double x,double y){
+                return x+y;
             }
       }
 
 public static void main(String[] args) { 
       Product p=new Product(); 
+        p.product(1,2);
         p.product(1,2,3);
+        p.product(1.1,2.2);
       //System.out.print(obj.meth());
     } 
 }
