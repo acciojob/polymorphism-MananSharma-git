875f0ca0df9ff6c6301e54fcaad6374cd912f5fe
MANAN SHARMA <115975287+MananSharma-git@users.noreply.github.com>
2023-05-04 00:17:37 +0530

    check1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,21 @@
-package com.driver;
 
 public class Main {
-      class A{
-      String meth(){ 
-          return "Invoking method from class A";
-        }
-    }
+  
+    static class Product{
+            public int product(int x,int y){
+                return x+y;
+            }
+            public double product(double x,double y){
+                return x+y;
+            }
+            public int product(int x,int y,int z){
+                return x+y+z;
+            }
+      }
 
-  class B extends A{ 
-      String meth(){ 
-          return "Method is overridden in Extendend class B"; 
-        } 
-  }
-
-  public class Main { public static void main(String[] args) { 
-      B obj=new B(); 
-      System.out.print(obj.meth());
+public static void main(String[] args) { 
+      Product p=new Product(); 
+        p.product(1,2,3);
+      //System.out.print(obj.meth());
     } 
 }
